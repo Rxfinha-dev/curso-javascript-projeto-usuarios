265ea5a7d651bda89a7926ecb3377a6fb38c2238
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2025-01-15 11:44:50 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked ");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var pswrd = document.querySelector("#exampleInputPassword");
+var image = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#inputAdmin");
+
+
+
+
+  
