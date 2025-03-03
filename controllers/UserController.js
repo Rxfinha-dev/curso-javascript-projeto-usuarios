e734d824b7d698b6d215484225f7e882d423726f
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2025-03-03 13:46:17 -0300

    Validating Forms

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -66,8 +66,19 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValid = true;
 
     [...this.formEl.elements].forEach((field, index) => {
+
+      if(['name', 'email', 'password'].indexOf(field.name)> -1 && !field.value ){
+
+        field.parentElement.classList.add('has-error');
+        isValid = false;
+
+      }
+
+
+
       if (field.name == "gender") {
         if (field.checked) {
           user[field.name] = field.value;
@@ -80,6 +91,10 @@ class UserController {
       }
 
     });
+
+    if(!isValid){
+      return false;
+    }
 
     return new User(
       user.name,
